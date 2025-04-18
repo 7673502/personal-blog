6542f9bed4f584bcdcb1fd79fe6d741dd8a15e0e
Pramod Anandarao <panandarao@wisc.edu>
2025-04-18 14:56:34 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Personal Blog
+
+This is my personal blog, created using the Astro [Barebones](https://github.com/superwebthemes/barebones) template.
+
---
+++ src/siteConfig.ts
@@ -0,0 +1,40 @@
+import type {
+  SiteConfiguration,
+  NavigationLinks,
+  SocialLinks,
+} from "@/types.ts";
+
+export const SITE: SiteConfiguration = {
+  title: "Pramod Anandarao",
+  description:
+    "A random assortment of thoughts",
+  href: "https://blog.pramodna.com",
+  author: "Pramod Anandarao",
+  locale: "en-US",
+};
+
+export const NAV_LINKS: NavigationLinks = {
+  blog: {
+    path: "/posts",
+    label: "Posts",
+  },
+};
+
+export const SOCIAL_LINKS: SocialLinks = {
+  github: {
+    label: "GitHub",
+    href: "https://github.com/7673502",
+  },
+  bluesky: {
+    label: "Bluesky",
+    href: "https://bsky.app/profile/pramod1.bsky.social",
+  },
+  instagram: {
+    label: "Instagram",
+    href: "https://www.instagram.com/pramodanandarao/",
+  },
+  linkedin: {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/pramod-anandarao-706690221/",
+  },
+};
